02c12a8a61883ea919e04fcd48a3bbc9196b7358
Matthews Gondwe <matthewgondwe1@gmail.com>
2025-03-25 15:48:01 +0200

    Added a new project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,16 +12,16 @@ const Footer = () => {
           <a href="https://www.linkedin.com/in/matthews-gondwe-429b35238/" target="_blank" rel="noopener noreferrer">
             <FaLinkedin className="text-2xl transition-transform transform hover:scale-110 hover:text-blue-500" />
           </a>
-          <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/Vitumbikoh" target="_blank" rel="noopener noreferrer">
             <FaGithub className="text-2xl transition-transform transform hover:scale-110 hover:text-gray-400" />
           </a>
           <a href="mailto:matthewgondwe1@gmail.com">
             <FaEnvelope className="text-2xl transition-transform transform hover:scale-110 hover:text-red-500" />
           </a>
-          <a href="https://twitter.com/your-twitter-handle" target="_blank" rel="noopener noreferrer">
+          <a href="https://twitter.com/MatthewChasowa" target="_blank" rel="noopener noreferrer">
             <FaTwitter className="text-2xl transition-transform transform hover:scale-110 hover:text-blue-400" />
           </a>
-          <a href="https://facebook.com/your-facebook-profile" target="_blank" rel="noopener noreferrer">
+          <a href="https://web.facebook.com/matthews.gondwe.18" target="_blank" rel="noopener noreferrer">
             <FaFacebook className="text-2xl transition-transform transform hover:scale-110 hover:text-blue-600" />
           </a>
         </div>
